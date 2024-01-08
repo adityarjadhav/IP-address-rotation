bd1d2ddfd0cc46c0146c61b3c1d54ac7acebe7b6
adityarjadhav <adityarjadhav@outlook.com>
2024-01-08 17:25:05 +0530

    added all essential files

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_instance" "my_instance" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  # allow_stopping_for_update = true
+}
